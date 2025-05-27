2e994a399f876141d411db1e20060ec1572a47a7
도일환 <ilhwan2babo@naver.com>
2025-05-27 11:47:38 +0900

    backup

--- src/components/textUtils.ts
+++
@@ -1,6 +0,0 @@
-// prettier-ignore
-export const makeClassName = 
-  function(setting: string,_className?: string,numberOfLines?: number) { 
-    return [setting, numberOfLines?`line-clamp-${numberOfLines}`:'', _className]
-    .join(' '); 
-  }
